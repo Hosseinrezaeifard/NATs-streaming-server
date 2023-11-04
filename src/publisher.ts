0e1f5abf3bf5a43070e5ddc0046853c008355aae
Hossein Rezaei Fard <57575843+Hosseinrezaeifard@users.noreply.github.com>
2023-11-04 08:44:35 -0700

    create publisher base class & leverage typescript with it

--- src/publisher.ts
+++ src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
@@ -6,14 +7,17 @@ const stan = nats.connect("ticketing", "abc", {
 
 console.clear();
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.info("publisher connected to nats!");
-  const data = JSON.stringify({
-    id: "123",
-    price: 20,
-    title: "concert",
-  });
-  stan.publish("ticket:created", data, () => {
-    console.log("event published");
-  });
+
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      price: 20,
+      title: "concert",
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
